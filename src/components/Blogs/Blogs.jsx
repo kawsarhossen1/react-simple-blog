f5cd5852ed487ae8e0461318214ee087fa8a174f
kawsarhossen1 <kawsarhossen66@gmail.com>
2025-01-06 15:18:36 +0600

    propTypes added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,21 +1,19 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
 
-    const [blogs, setBlogs] = useState([]);
-
-    useEffect( () =>{
-        fetch('blogs.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    }, [])
-    return (
-        <div>
-            
-        </div>
-    );
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  return (
+    <div className="md:w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
+import profile from "../../assets/images/profile.png";
+
 const Header = () => {
   return (
-    <div>
+    <header className="flex  justify-between items-center p-4 mx-4 border-b-2">
       <h1 className="text-4xl font-bold">React simple vlog</h1>
-    </div>
+      <img src={profile} alt="" />
+    </header>
   );
 };
 
